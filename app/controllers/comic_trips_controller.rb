49673adf9d9980610f8e10f93573a89ccfecdcb8
Romain Bruzeau <bruzeau.romain@gmail.com>
2024-03-06 15:16:43 +0100

    preview02

--- app/controllers/comic_trips_controller.rb
+++ app/controllers/comic_trips_controller.rb
@@ -1,5 +1,5 @@
 class ComicTripsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :new ]
 
   def home
     @comic_trips = ComicTrip.all
