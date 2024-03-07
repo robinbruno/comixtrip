1f058a86133040ab81ebd3bf5a0bf9912939d49f
Romain Bruzeau <bruzeau.romain@gmail.com>
2024-03-07 17:47:21 +0100

    preview-mix

--- app/javascript/controllers/preview.js
+++ app/javascript/controllers/preview.js
@@ -1,12 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Create const from _buro partial
-  const inputField = document.querySelector('[id or class="ADD ID FROM KALEK"]');
-  // Create const from _preview partial (first vignette)
+  const inputField = document.querySelector('[id ="titleInput"]');
+  // Create const from _preview partial title
+  const emptyField = document.querySelector('.comic-title');
+
+  if (!inputField) {
+    console.log("Input field empty");
+  } else {
+    // Add event listener to input field on _buro partial
+    inputField.addEventListener('input', function() {
+      // Get the value from _buro partial
+      const inputValue = inputField.value;
+
+      // Write the value to the empty field on _preview
+      emptyField.textContent = inputValue;
+
+      // Remove "d-none" class from emptyField
+      emptyField.classList.remove("d-none");
+    });
+  }
+});
+
+  document.addEventListener("DOMContentLoaded", function() {
+  // Create const from _buro partial
+  const inputField = document.querySelector('[id ="dialogueInput1"]');
+  // Create const from _preview partial title
   const emptyField = document.querySelector('.rectangle-left');
 
-  // Check if inputField exists
   if (!inputField) {
-    console.log("Input field not found");
+    console.log("Input field empty");
+  } else {
+    // Add event listener to input field on _buro partial
+    inputField.addEventListener('input', function() {
+      // Get the value from _buro partial
+      const inputValue = inputField.value;
+
+      // Write the value to the empty field on _preview
+      emptyField.textContent = inputValue;
+
+      // Remove "d-none" class from emptyField
+      emptyField.classList.remove("d-none");
+    });
+  }
+});
+
+  document.addEventListener("DOMContentLoaded", function() {
+  // Create const from _buro partial
+  const inputField = document.querySelector('[id ="dialogueInput2"]');
+  // Create const from _preview partial title
+  const emptyField = document.querySelector('.rectangle-right');
+
+  if (!inputField) {
+    console.log("Input field empty");
   } else {
     // Add event listener to input field on _buro partial
     inputField.addEventListener('input', function() {
