459a276d6395d6e0b6d427a83376170050faa88e
Romain Bruzeau <bruzeau.romain@gmail.com>
2024-03-11 14:30:40 +0100

    text01-02-vignettes-table

--- app/controllers/comic_trips_controller.rb
+++ app/controllers/comic_trips_controller.rb
@@ -1,5 +1,5 @@
 class ComicTripsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :new ]
+  skip_before_action :authenticate_user!, only: [ :index, :new, :create, :update ]
 
   def home
     @comic_trips = ComicTrip.all
@@ -11,17 +11,27 @@ class ComicTripsController < ApplicationController
     @comic_trips = ComicTrip.all
   end
 
-
   def show
     @comic_trip = ComicTrip.find(params[:id])
   end
 
   def new
-    @comic_trips = ComicTrip.new
+    @comic_trip = ComicTrip.new
   end
 
   def create
+    p "hello from comic_trips_controller"
+    # vignette1, vignette2, vignette3
+    @comic_trip = ComicTrip.new(comic_params)
+    @vignette1 = Vignette.new(vignette1_params)
+    @comic_trip.user = current_user
+    if @comic_trip.save
+    if @vignette1.save
+      redirect_to   ,notice: "Vignette was successfully created."
 
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -32,6 +42,16 @@ class ComicTripsController < ApplicationController
 
   end
 
+  private
+
+  def comic_params
+    params.require(:comic).permit(:title, :dialogue1, :dialogue2)
+  end
+
+  def vignette1_params
+    params.require(:vignette1).permit(:dialogue1, :dialogue2)
+
+  end
 
 
 end
--- app/controllers/vignettes_controller.rb
+++ app/controllers/vignettes_controller.rb
@@ -1,2 +1,31 @@
 class VignettesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :new, :create, :update ]
+
+
+  def new
+    @vignette = Vignette.new
+  end
+
+  def create
+    p "hello from Vignette"
+    @vignette = Vignette.new(vignette_params)
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def vignette_params
+    params.require(:vignette).permit(:text)
+  end
+
+
+
 end
