c2f45ff6660cd1dcf2638f9e7e6376d81ea4aad4
Abdelkalek Chahed <ka.chahed@gmail.com>
2024-03-12 15:15:45 +0100

    preview2

--- app/javascript/controllers/live_preview_controller.js
+++ app/javascript/controllers/live_preview_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="live-preview"
 export default class extends Controller {
-  static targets = ["rectangleLeft1", "rectangleRight1", "comicTitle", "rectangleLeft2", "rectangleRight2", "rectangleLeft3", "rectangleRight3"]
+  static targets = ["rectangleLeft1", "rectangleRight1", "comicTitle", "rectangleLeft2", "rectangleRight2", "rectangleLeft3", "rectangleRight3", "vignetteLeft", "vignetteCenter", "vignetteRight"]
 
   connect() {
   }
