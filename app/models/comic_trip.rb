055cd6b1473d31d2fe65e08cca8362a96ee5b6a7
Romain Bruzeau <bruzeau.romain@gmail.com>
2024-03-15 11:37:24 +0100

    dependent destroy

--- app/models/comic_trip.rb
+++ app/models/comic_trip.rb
@@ -1,7 +1,7 @@
 class ComicTrip < ApplicationRecord
   belongs_to :user
   has_many :albums, through: :album_comic_trips
-  has_many :vignettes
-  has_many :bookmarks
- 
+  has_many :vignettes, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+
 end
